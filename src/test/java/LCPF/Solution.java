91203b0872475e52ef4a0716816fd7720ab2316c
Ivangorno <111599129+Ivangorno@users.noreply.github.com>
2022-09-13 00:34:18 +0300

    added ArrayList of words to compare (instead of just three words)

--- src/test/java/LCPF/Solution.java
+++ src/test/java/LCPF/Solution.java
@@ -5,40 +5,52 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-
-    static String s1;
-    static String s2;
-    static String s3;
+  static ArrayList<String> listOfWords = new ArrayList<>();
+  static String word;
 
     private static void input() {
 
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter the first word: ");
-        s1 = input.nextLine();
-        System.out.println("Enter the second word: ");
-        s2 = input.nextLine();
-        System.out.println("Enter the third word: ");
-        s3 = input.nextLine();
+        Scanner input = new Scanner(System.in);
+        listOfWords.add(input.nextLine());
+
+        System.out.println("Enter another word to compere OR enter \"any number\" to stop: ");
+        while (!input.hasNextInt()){
+            System.out.println("Enter another word to compere OR enter \"any number\" to stop: ");
+            word  = input.nextLine();
+            listOfWords.add(word);}
+
     }
     public static String findPrefix() {
 
-        StringBuilder commonPrefix = new StringBuilder();
-        int shortestStr = s1.length();
+       StringBuilder commonPrefix = new StringBuilder();
 
-        if (s1.length() > s2.length() && s2.length() < s3.length()) {
-            shortestStr = s2.length();
-        } else if (s1.length() > s3.length() && s2.length() > s3.length()) {
-            shortestStr = s3.length();
-        }
+        String shortestWord = "";
 
-        for (int i = 0; i < shortestStr; i++) {
-            if (s1.charAt(i) == s2.charAt(i) && s2.charAt(i) == s3.charAt(i)) {
-                commonPrefix.append(s1.charAt(i));
-            } else {
-                break;
+        for( String word : listOfWords ){
+            if (shortestWord.length() ==0  && word.length() !=0  )
+            {
+                shortestWord = word;
+            } else if(shortestWord.length() > word.length() && word.length() != 0  ){
+
             }
-        }
-        return commonPrefix.toString();
+            shortestWord = word;
+            }
+
+
+
+      for (int i = 0; i< shortestWord.length();i++){
+          char prefix = listOfWords.get(0).charAt(i);
+          for (String word : listOfWords) {
+              if (word.charAt(i) != prefix){
+                  return commonPrefix.toString();
+              }
+          }
+          commonPrefix.append(prefix);
+      }
+
+    return  commonPrefix.toString();
+
     }
 
     public static void main(String[] args) {
